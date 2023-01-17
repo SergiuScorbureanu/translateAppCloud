5848c92ae01978502d329f7d7ba465050e1e4f86
SergiuScorbureanu <sergiu.scorbureanu@gmail.com>
2023-01-17 19:36:58 +0200

    Initial commit

---
+++ src/main/java/com/translateCloud/translateCloud/TranslateCloudApplication.java
@@ -0,0 +1,13 @@
+package com.translateCloud.translateCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslateCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranslateCloudApplication.class, args);
+	}
+
+}
